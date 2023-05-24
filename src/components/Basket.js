fdd0c78fbb7cd6489eddc0cd78d61510a68ed62e
Lukas G <luka9938@stud.kea.dk>
2023-05-24 15:33:43 +0200

    price added

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { StoreContext } from "@/contexts/basketContext";
 import { ticketTypes } from "@/data/ticketTypes";
 import { tentTypes } from "@/data/tentTypes";
@@ -8,6 +8,44 @@ import Link from "next/link";
 
 function Basket() {
   const state = useContext(StoreContext);
+  const [additionalCost, setAdditionalCost] = useState(0);
+
+  // Calculate the total price
+  const calculateTotalPrice = () => {
+    let totalPrice = 100;
+
+    // Calculate ticket prices
+    state.ticketBasket.forEach((item) => {
+      const ticketType = ticketTypes.find((type) => type.id === item.id);
+      totalPrice += ticketType.price * item.amount;
+    });
+
+    // Calculate tent prices
+    state.tentBasket.forEach((item) => {
+      const tentType = tentTypes.find((type) => type.id === item.id);
+      totalPrice += tentType.price * item.amount;
+    });
+
+    // Add additional cost if checkbox is checked
+    totalPrice += additionalCost;
+
+    return totalPrice;
+  };
+
+  // Check if all baskets are empty
+  const isBasketEmpty = () => {
+    return (
+      state.ticketBasket.length === 0 ||
+      state.tentBasket.length === 0 ||
+      state.campBasket.length === 0
+    );
+  };
+
+  const handleCheckboxChange = (e) => {
+    const isChecked = e.target.checked;
+    const additionalCostValue = isChecked ? 250 : 0;
+    setAdditionalCost(additionalCostValue);
+  };
 
   return (
     <div className="Basket">
@@ -20,25 +58,41 @@ function Basket() {
           );
         })}
       </ul>
+      <h2>Your Tents</h2>
       <ul>
         {state.tentBasket.map((item) => {
           const tentType = tentTypes.find((type) => type.id === item.id);
           return (
-            <BasketCart key={item.id} {...tentType} amount={item.amount} />
+            <BasketCart
+              key={item.id}
+              {...tentType}
+              amount={item.amount}
+              ticketCount={state.ticketBasket.length}
+            />
           );
         })}
       </ul>
+      <h2>Your Camps</h2>
       <ul>
         {state.campBasket.map((item) => {
-          return <CartCamp {...item} />;
+          return <CartCamp key={item.id} {...item} />;
         })}
       </ul>
+      <ul>
+        <label>
+          <input
+            type="checkbox"
+            onChange={handleCheckboxChange}
+            checked={additionalCost > 0}
+          />
+          Green camping (250,-)
+        </label>
+      </ul>
+      <p>Total Price: ${calculateTotalPrice()}</p>
       <Link
         href="./tents"
-        className={`button ${
-          state.ticketBasket.length === 0 ? "disabled" : ""
-        }`}
-        disabled={state.ticketBasket.length === 0}
+        className={`button ${isBasketEmpty() ? "disabled" : ""}`}
+        disabled={isBasketEmpty()}
       >
         Continue
       </Link>
