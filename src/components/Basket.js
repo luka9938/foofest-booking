f2aa5652f73adbb915cb3d975275826e8c7ba805
Lukas G <luka9938@stud.kea.dk>
2023-06-01 17:50:29 +0200

    code_cleaning

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import { StoreContext } from "@/contexts/basketContext";
 import { ticketTypes } from "@/data/ticketTypes";
 import { tentTypes } from "@/data/tentTypes";
@@ -7,36 +7,6 @@ import CartCamp from "./CartCamp";
 
 function Basket() {
   const state = useContext(StoreContext);
-  const [additionalCost, setAdditionalCost] = useState(0);
-
-  // Calculate the total price
-  const calculateTotalPrice = () => {
-    let totalPrice = 99;
-
-    // Calculate ticket prices
-    state.ticketBasket.forEach((item) => {
-      const ticketType = ticketTypes.find((type) => type.id === item.id);
-      totalPrice += ticketType.price * item.amount;
-    });
-
-    // Calculate tent prices
-    state.tentBasket.forEach((item) => {
-      const tentType = tentTypes.find((type) => type.id === item.id);
-      totalPrice += tentType.price * item.amount;
-    });
-
-    // Add additional cost if checkbox is checked
-    totalPrice += additionalCost;
-
-    return totalPrice;
-  };
-
-  const handleCheckboxChange = (e) => {
-    const isChecked = e.target.checked;
-    const additionalCostValue = isChecked ? 249 : 0;
-    setAdditionalCost(additionalCostValue);
-  };
-
   return (
     <>
       <h3>Your Tickets</h3>
